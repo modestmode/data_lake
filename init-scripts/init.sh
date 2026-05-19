1f0fce2b1cc610275f1fe66738535616f2928749
Rakhat Shalgynbay <rakhatshalgynbay@gmail.com>
2026-05-19 16:46:23 +0500

    first commit

---
+++ init-scripts/init.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+
+# 1. Create the second database
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE minio_metastore;
+EOSQL
+
+# 2. Initialize tables in iceberg_metastore (automatically created by POSTGRES_DB)
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "iceberg_metastore" <<-EOSQL
+  CREATE TABLE IF NOT EXISTS iceberg_tables (
+    catalog_name VARCHAR(255) NOT NULL,
+    table_namespace VARCHAR(255) NOT NULL,
+    table_name VARCHAR(255) NOT NULL,
+    metadata_location VARCHAR(1000),
+    previous_metadata_location VARCHAR(1000),
+    PRIMARY KEY (catalog_name, table_namespace, table_name)
+  );
+  CREATE TABLE IF NOT EXISTS iceberg_namespace_properties (
+    catalog_name VARCHAR(255) NOT NULL,
+    namespace VARCHAR(255) NOT NULL,
+    property_key VARCHAR(255),
+    property_value VARCHAR(1000),
+    PRIMARY KEY (catalog_name, namespace, property_key)
+  );
+EOSQL
+
+# 3. Initialize tables in minio_metastore
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "minio_metastore" <<-EOSQL
+  CREATE TABLE IF NOT EXISTS iceberg_tables (
+    catalog_name VARCHAR(255) NOT NULL,
+    table_namespace VARCHAR(255) NOT NULL,
+    table_name VARCHAR(255) NOT NULL,
+    metadata_location VARCHAR(1000),
+    previous_metadata_location VARCHAR(1000),
+    PRIMARY KEY (catalog_name, table_namespace, table_name)
+  );
+  CREATE TABLE IF NOT EXISTS iceberg_namespace_properties (
+    catalog_name VARCHAR(255) NOT NULL,
+    namespace VARCHAR(255) NOT NULL,
+    property_key VARCHAR(255),
+    property_value VARCHAR(1000),
+    PRIMARY KEY (catalog_name, namespace, property_key)
+  );
+EOSQL
